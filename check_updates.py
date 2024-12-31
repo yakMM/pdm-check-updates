b54e50b66fb6c0be1c5845a560e44ec452127abf
YakMM <yak.mayam@gmail.com>
2024-12-31 11:16:02 +0100

    Update plugin

--- check_updates.py
+++ check_updates.py
@@ -1,4 +1,4 @@
-""""Quick and dirty pdm plugin to check for available updates from Pypi"""
+""" "Quick and dirty pdm plugin to check for available updates from Pypi"""
 
 import argparse
 
@@ -13,45 +13,29 @@ class CheckCommand(BaseCommand):
 
     def handle(self, project: Project, _: argparse.Namespace) -> None:
         for group in project.iter_groups():
-            deps = project.get_dependencies(group)
-            for dep in deps.values():
+            for dep in project.get_dependencies(group):
                 if dep.is_pinned and not dep.is_vcs and not dep.is_file_or_url:
                     first = None
                     info = False
                     dep_version = parse(str(dep.specifier).replace("=", ""))
-                    for candidate in project.get_repository()._find_candidates(
-                        dep, minimal_version=False
-                    ):
+                    for candidate in project.get_repository()._find_candidates(dep, minimal_version=False):
                         if not candidate.version:
                             raise ValueError(f"Invalid candidate: {candidate.name}")
                         version = parse(candidate.version)
-                        if not version.is_prerelease or (
-                            version.is_prerelease and dep_version.is_prerelease
-                        ):
+                        if not version.is_prerelease or (version.is_prerelease and dep_version.is_prerelease):
                             first = candidate
                             break
-                        elif not info:
+                        if not info:
                             info = True
-                            print(
-                                f"Info: pre-release available for {dep.name}: {version}"
-                            )
+                            print(f"Info: pre-release available for {dep.name}: {version}")
                     if not first:
-                        raise ValueError(
-                            f"No matching candidate found for package {dep.name}"
-                        )
+                        raise ValueError(f"No matching candidate found for package {dep.name}")
                     if first.version != str(dep_version):
                         print(
-                            f"Package '{dep.name}' can be updated: "
-                            f"'{dep_version}' => '{first.version}'"
+                            f"Package '{dep.name}' can be updated: " f"'{dep_version}' => '{first.version}'"
                         )
                 else:
-                    first = next(
-                        iter(
-                            project.get_repository()._find_candidates(
-                                dep, minimal_version=False
-                            )
-                        )
-                    )
+                    first = next(iter(project.get_repository()._find_candidates(dep, minimal_version=False)))
                     if not first.version:
                         raise ValueError(f"Invalid candidate: {first.name}")
                     version = parse(first.version)
